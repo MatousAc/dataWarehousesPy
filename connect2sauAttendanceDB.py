fb68030cf59edf866e1140f4492a22c235abbbc9
MatousAc <matoush@southern.edu>
2022-11-07 00:41:59 -0500

    basic pyodbc connection

--- connect2sauAttendanceDB.py
+++ connect2sauAttendanceDB.py
@@ -1,6 +1,7 @@
 import pyodbc 
 cnxn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:matousacserver.database.windows.net,1433;Database=attendanceDB;Uid={matoush@southern.edu};Pwd={while S in range(5):};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;Authentication=ActiveDirectoryPassword')
 
+
 cursor = cnxn.cursor()	
 cursor.execute("select * from room") 
 row = cursor.fetchone() 
